c370e91a84847e342ff906ce4e8e4a4c37f56658
Vitor Gabriel <vs569119@gmail.com>
2024-09-28 13:55:00 -0300

    Função de gerar QR Code adicionada

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,27 @@
-console.log("teste");
+const container = document.querySelector("#container");
+const qrCodeInput = document.querySelector("#url");
+const qrCodeBtn = document.querySelector("#generate-btn");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+
+const generateQrCode = () => {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) {
+        return;
+    }
+
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código gerado!";
+    });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
